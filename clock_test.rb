b81a52c9c598ffd87d1b7cf740b9430c5ed1d99e
Tony DiNitto <think41c@gmail.com>
2015-05-17 23:19:40 -0400

    Initial tests passing.

--- clock_test.rb
+++ clock_test.rb
@@ -4,7 +4,6 @@ require_relative 'clock'
 
 class ClockTest < Minitest::Test
   def test_on_the_hour
-    skip
     assert_equal '08:00', Clock.at(8).to_s
     assert_equal '09:00', Clock.at(9).to_s
   end
