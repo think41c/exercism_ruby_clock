f8814ac46d3bbdf9fe1ef1f9a6d16eade6d0e800
Tony DiNitto <think41c@gmail.com>
2015-05-21 11:59:50 -0400

    Completed - but will come back to review.

--- clock.rb
+++ clock.rb
@@ -1,23 +1,38 @@
+# I had extensive help with this Exercism. I submitted this
+# because there's no way to advance to another Exercism until
+# one is submitted. Any thoughts/tips/etc on what is going on 
+# in regards to the manipulation of 'self' and so on is greatly
+# appreciated.
+
 class Clock
 
-  def initialize(x)
+  def self.at(hour, min = 0)
+    minutes = hour * 60 + min
+    new(minutes)
   end
 
-  def +(min)
-    puts "here?"
-    self.class.new(@time + sec * 60)
+  def initialize(minutes)
+    @minutes = minutes
   end
-  
-  def self.at(hour, min=0)
-    puts hour
-    puts min
-    new(min)
-    hour = "#{hour}"
-    min  = "#{min}"
-    hour_formatted = hour.rjust(2,"0")
-    a = "#{hour_formatted}:#{min.rjust(2,"0")}"
-    a
+
+  def +(add_minutes)
+    puts add_minutes
+    self.class.new(@minutes + add_minutes)
+  end
+
+  def -(subtract_minutes)
+    self.+(-subtract_minutes)
+  end
+
+  def ==(other)
+    hour_and_minute == other.hour_and_minute
+  end
+
+  def to_s
+    "%02d:%02d" % hour_and_minute
+  end
+
+  def hour_and_minute
+    [ @minutes / 60 % 24, @minutes % 60 ]
   end
 end
-p Clock.at(11)
-
